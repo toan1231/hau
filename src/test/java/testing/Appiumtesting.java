2313c6a7d6cb18c0b662546d425b24de4bf2566d
chaut <chauthai2003@gmail.com>
2023-01-27 11:38:24 -0500

    I change from Android<AndroidElement> to Appium<WebElement>

--- src/test/java/testing/Appiumtesting.java
+++ src/test/java/testing/Appiumtesting.java
@@ -37,8 +37,7 @@ public class Appiumtesting {
         el3.click();
         MobileElement el4 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout");
         el4.click();
-        MobileElement el5 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout");
-        el5.click();
+
 
     }
 }
